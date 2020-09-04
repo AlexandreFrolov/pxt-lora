fe29ad3a646ea84a78a0fc3e9794e817285aa97a
Александр Фролов <frolov@itmatrix.ru>
2020-09-04 09:22:10 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
+/**
+ * pxt-lora block
+ */
+//% weight=100 color=#00cc00 icon="\uf012" block="E32LORA"
+namespace pxtlora {
+
+//    const E32LORA_I2C_ADDR=0x68
+
+
+    function E32LORA_init() {
+    }
+}
 
